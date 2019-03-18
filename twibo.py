d7633695119b89f2bf4909ce4af6d211118ac374
zhangjiangqige <zhangjiangqige@gmail.com>
2019-03-18 18:37:07 -0400

    with supervisord and nginx

--- twibo.py
+++ twibo.py
@@ -1,11 +1,19 @@
+import argparse
 from aiohttp import web
 
 from views import index
 
 
-app = web.Application()
-app.add_routes([
-    web.get('/', index.hello)
-])
+parser = argparse.ArgumentParser(description="aiohttp server example")
+parser.add_argument('--path', default=None)
+parser.add_argument('--port', default=None)
 
-web.run_app(app)
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([
+        web.get('/', index.hello)
+    ])
+
+    args = parser.parse_args()
+    web.run_app(app, path=args.path, port=args.port)
