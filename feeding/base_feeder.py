610997860bfa475a2d075da2d03bd8d4ca38838a
zhangjiangqige <zhangjiangqige@gmail.com>
2019-03-23 16:10:22 -0400

    support lock in basefeeder

--- feeding/base_feeder.py
+++ feeding/base_feeder.py
@@ -1,4 +1,6 @@
 from background import background_tasks
+from caching.caching_utils import redis
+
 
 class BaseFeeder:
     _prefix = 'twibo'
@@ -9,6 +11,13 @@ class BaseFeeder:
 
     def add_background_task(self, name, coro):
         background_tasks.append((name, coro))
+
+    async def lock(self, lockname):
+        # non-blocking; returns true if locked
+        return await redis.set(self.get_key(lockname), '1', exist=redis.SET_IF_NOT_EXIST)
+
+    async def unlock(self, lockname):
+        return await redis.delete(self.get_key(lockname))
 
     async def create(self, user_id, tweet_id, content, timestamp, **kwargs):
         raise NotImplementedError
