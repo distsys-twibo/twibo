d1d70e6471a3557cc7630622ce1da391cfbfd335
zhangjiangqige <zhangjiangqige@gmail.com>
2019-03-26 15:48:36 -0400

    separate printing get and create

--- load-test/run_single.py
+++ load-test/run_single.py
@@ -31,7 +31,8 @@ def main():
     print('begin. prefix of tweet_id is {}'.format(tid_prefix))
     t0 = time.time()
     c = 0
-    calcers = defaultdict(utils.Calcer)
+    calcers_get = defaultdict(utils.Calcer)
+    calcers_create = defaultdict(utils.Calcer)
     for ts, action, user_id, tlen in activities:
         if action == 'Retrieve':
             resp = requests.get(url_get, params={
@@ -39,6 +40,8 @@ def main():
                 'limit': 50,
                 'pop': 0
             })
+            respj = resp.json()
+            utils.save_timer(calcers_get, respj['timer'])
         else:
             resp = requests.post(url_create, data={
                 'tweet_id': tid_prefix + '-' + str(ts),
@@ -46,16 +49,19 @@ def main():
                 'ts': ts,
                 'content': utils.random_string(tlen)
             })
-        respj = resp.json()
-        # pprint(respj)
-        utils.save_timer(calcers, respj['timer'])
+            respj = resp.json()
+            utils.save_timer(calcers_create, respj['timer'])
+        pprint(respj)
         c += 1
         if c % 100 == 0:
             print('progress:', time.time() - t0, c, c / (time.time() - t0))
     t999 = time.time()
     duration = t999 - t0
     print('finished. {:.2f}, {:.2f}/s, {:.5f}/req'.format(duration, num / duration, duration / num))
-    utils.print_calcers(calcers)
+    print('---timers of get---')
+    utils.print_calcers(calcers_get)
+    print('---timers of create---')
+    utils.print_calcers(calcers_create)
 
 if __name__ == "__main__":
     main()
