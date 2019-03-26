9051adb8a73199ed7775339a4d5bc9ea5c83bc44
zhangjiangqige <zhangjiangqige@gmail.com>
2019-03-26 14:38:32 -0400

    timer and pull

--- feeding/feed_pull.py
+++ feeding/feed_pull.py
@@ -1,5 +1,6 @@
 import logging
 import heapq
+import time
 
 from db import tweet, user_follow
 from .base_feeder import BaseFeeder
@@ -17,15 +18,28 @@ class FeedPull(BaseFeeder):
     async def create(self, user_id, tweet_id, content, timestamp, **kwargs):
         logger.debug('user_id {} tweet_id {} ts {}'.format(
             user_id, tweet_id, timestamp))
-        return await tweet.create(user_id, tweet_id, content, timestamp)
+        timer = kwargs.get('timer', {})
+        t0 = time.time()
+        ret = await tweet.create(user_id, tweet_id, content, timestamp)
+        t1 = time.time()
+        timer['db_create'] = t1 - t0
+        return ret
 
     async def get(self, user_id, limit, **kwargs):
         logger.debug('user_id {} limit {}'.format(user_id, limit))
+        timer = kwargs.get('timer', {})
+        t0 = time.time()
         followees = await user_follow.all_followees(user_id)
+        t1 = time.time()
         feeds = []
         # get @limit feeds from each of the followees
         # then get the newest @limit feeds among them
         for fle in followees:
             feeds += await tweet.get_by_user_id(fle, limit)
+        t2 = time.time()
         feeds = heapq.nlargest(limit, feeds, key=lambda x: x['ts'])
+        t3 = time.time()
+        timer['db_get_rel_followee'] = t1 - t0
+        timer['db_get_feeds'] = t2 - t1
+        timer['sort'] = t3 - t2
         return feeds
--- views/tweet.py
+++ views/tweet.py
@@ -23,13 +23,13 @@ async def create(request):
     tweet_id = data['tweet_id']
     content = data['content']
     timestamp = float(data['ts'])
+    timer = {}
     t1 = time.time()
-    await feeder.create(user_id, tweet_id, content, timestamp)
+    await feeder.create(user_id, tweet_id, content, timestamp, timer=timer)
     t2 = time.time()
+    timer['full'] = t2 - t1
     return web.json_response({
-        'times': {
-            'full': t2 - t1
-        }
+        'timer': timer
     })
 
 
@@ -39,12 +39,12 @@ async def get(request):
     user_id = query['user_id']
     limit = int(query.get('limit', 10))
     pop = bool(int(query.get('pop', 0)))
+    timer = {}
     t1 = time.time()
-    tweets = await feeder.get(user_id, limit, pop=pop)
+    tweets = await feeder.get(user_id, limit, pop=pop, timer=timer)
     t2 = time.time()
+    timer['full'] = t2 - t1
     return web.json_response({
         'tweets': tweets,
-        'times': {
-            'full': t2 - t1
-        }
+        'timer': timer
     })
