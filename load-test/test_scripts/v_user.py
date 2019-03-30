b2953b8bc34a51ca6ec5fb1ab910947782d28717
zhangjiangqige <zhangjiangqige@gmail.com>
2019-03-30 17:01:46 -0400

    fix dup read and print exception

--- load-test/test_scripts/v_user.py
+++ load-test/test_scripts/v_user.py
@@ -14,32 +14,33 @@ url_base = 'http://127.0.0.1:9991'
 url_create = url_base + '/tweet/create'
 url_get = url_base + '/tweet/get'
 # path to the output of SONETOR
-activity_file = ''
+activity_file = '/u7/j892zhang/mem/twibo/8w-sorted.txt'
 # max length of a tweet
 t_maxlen = 400
+
+f = []
+n_processes = 100
+if n_processes == 1:
+    f.append(open(activity_file))
+else:
+    # exhaustively open 100 input files...
+    try:
+        for i in range(n_processes):
+            f.append(open(activity_file + '.{}'.format(i)))
+    except:
+        pass
+print 'found {} input files'.format(len(f))
 
 
 class Transaction(object):
     def __init__(self, n_processes=None):
-        self.f = []
-        n_processes = n_processes if n_processes is not None else 100
-        if n_processes == 1:
-            self.f.append(open(activity_file))
-        else:
-            # exhaustively open 100 input files...
-            try:
-                for i in range(n_processes):
-                    self.f.append(open(activity_file + '.{}'.format(i)))
-            except:
-                pass
-        print 'found {} input files'.format(len(self.f))
         self.tid_prefix = socket.gethostname()
         self.s = requests.session()
         # will be overriden by multimechnize
         self.process_num = 0
 
     def next_act(self):
-        l = self.f[self.process_num].readline()
+        l = f[self.process_num].readline()
         act = utils.parse_line(l)
         utils.norm_act(act, 1, t_maxlen)
         return act
@@ -53,9 +54,7 @@ class Transaction(object):
                 'limit': 50,
                 'pop': 0
             })
-            respj = resp.json()
-            for k, v in respj['timer'].iteritems():
-                self.custom_timers['get_{}'.format(k)] = v
+            pref = 'get'
         else:
             resp = self.s.post(url_create, data={
                 'tweet_id': self.tid_prefix + '-' + str(ts) + '-' + utils.random_string(6),
@@ -63,9 +62,15 @@ class Transaction(object):
                 'ts': ts,
                 'content': utils.random_string(tlen)
             })
+            pref = 'create'
+
+        try:
             respj = resp.json()
             for k, v in respj['timer'].iteritems():
-                self.custom_timers['create_{}'.format(k)] = v
+                self.custom_timers['{}_{}'.format(pref, k)] = v
+        except Exception as e:
+            print 'exception: {}. {} {} {} {}\nresp:{}: {}'.format(e, ts, action, user_id, tlen, resp.status_code, resp.text)
+            raise e
 
 
 if __name__ == '__main__':
